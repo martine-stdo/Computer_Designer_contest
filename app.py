b1056b4ed87178ee9943b57ed6a5ed2358376f55
night-dreamer <nighterdrea123@gmail.com>
2023-04-24 09:54:58 +0800

    重大更新，将用户信息存储于sqlite中取代了原本文本读写

--- app.py
+++ app.py
@@ -4,11 +4,21 @@ import json
 import hashlib
 import pandas as pd
 from fuzzywuzzy import fuzz, process
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__,static_folder='static')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///user.db'
+db = SQLAlchemy(app)
+
 app.secret_key = 'jfsifsihfih'
 
 cookie = "dfjsiedhfuhuefheufhewf"
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(200), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+
 
 def read_credentials(file_path):
     credentials = {}
@@ -33,49 +43,50 @@ def error_404():
     return render_template('error.html')
 
 # 登录页面，与文本核对是否一致，一致则返回cookie，空json
-@app.route('/admin/login', methods=['POST','GET'])
-def login():  # put application's code here
-    credentials = read_credentials('user.txt')
-
+@app.route('/admin/login', methods=['POST', 'GET'])
+def login():
     if request.method == 'POST':
         json_data = request.data.decode('utf-8')
-
         data = json.loads(json_data)
-        # 从字典中获取用户名和密码
         username = data.get('username')
         password = data.get('passwd')
-        response = {"status":False, "cookie": ""}
-        if username in credentials:
-            if password == credentials[username]:
-                response = {"status":True, "cookie": cookie}
-        
+        response = {"status": False, "cookie": ""}
+
+        # 从数据库中查询用户凭据
+        user = User.query.filter_by(username=username).first()
+
+        # 如果用户存在且密码匹配，则返回成功响应和cookie
+        if user and user.password == password:
+            response = {"status": True, "cookie": cookie}
+
         my_json = json.dumps(response)
         return my_json
-        
     else:
         return render_template('login.html')
+
     
 #注册页面
-@app.route('/admin/regist', methods=['POST','GET'])
+@app.route('/admin/regist', methods=['POST', 'GET'])
 def register():
     if request.method == 'POST':
-        # 从表单中获取用户名和密码
         username = request.form['username']
         password = request.form['pwd1']
-        
+
         # 检查用户名是否已存在
-        with open('user.txt', 'r') as f:
-            credentials = [line.strip().split(',') for line in f.readlines()]
-        if username in [user[0] for user in credentials]:
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user is not None:
             return '用户名已存在'
-        
-        # 将用户名和密码写入user.txt文件中
-        with open('user.txt', 'a') as f:
-            f.write(username + ',' + password + '\n')
+
+        # 将用户名和密码添加到User模型并保存到数据库
+        new_user = User(username=username, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+
         flash('注册成功，请登录')
         return render_template('login.html')
     else:
         return render_template('register.html')
+
 #联系我们界面
 @app.route('/contactme', methods=['GET'])
 def contact():
@@ -268,4 +279,6 @@ def index():
 
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(port=5000, debug=True)
